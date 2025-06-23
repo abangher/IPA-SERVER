ec43e6d40e93a1b7c3ea77752a31c857bdb9e297
Hermanto <info.hermanto@gmail.com>
2025-06-23 15:58:00 +0700

    Update userlist.csv+script.sh

--- userlist.csv+script.sh
+++ userlist.csv+script.sh
@@ -8,13 +8,24 @@
 
 
 ### script.sh #####
+#!/bin/bash
+
 PASSWORD="P@ssw0rd123"
 
 while IFS= read -r email; do
-  username="${email%@*}"
+  username="${email%@*}"                  # hermanto.ganteng
+  firstname="${username%%.*}"             # Hermanto
+  lastname="${username#*.}"               # Ganteng
+
+  # Kapitalisasi huruf pertama
+  fname_cap="${firstname^}"               # Hermanto
+  lname_cap="${lastname^}"                # Ganteng
+
+  echo "Menambahkan user: $username ($fname_cap $lname_cap)"
+
   ipa user-add "$username" \
-    --first="User" \
-    --last="$username" \
+    --first="$fname_cap" \
+    --last="$lname_cap" \
     --email="$email" \
     --password <<EOF
 $PASSWORD
