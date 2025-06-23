b59a707cc4c4d4d35e07163a806e602cf46238e5
Hermanto <info.hermanto@gmail.com>
2025-06-23 15:14:44 +0700

    Create userlist.csv+script.sh

---
+++ userlist.csv+script.sh
@@ -0,0 +1,20 @@
+### create userlist.csv ####
+# -hermanto@ .....
+# -vernandos@.....
+# -reobertos@....
+
+
+### script.sh #####
+PASSWORD="P@ssw0rd123"
+
+while IFS= read -r email; do
+  username="${email%@*}"
+  ipa user-add "$username" \
+    --first="User" \
+    --last="$username" \
+    --email="$email" \
+    --password <<EOF
+$PASSWORD
+$PASSWORD
+EOF
+done < userlist.csv
