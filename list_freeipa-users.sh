40b2bab46b224d6167bac8954682473b041c46a3
Hermanto <info.hermanto@gmail.com>
2025-06-30 11:05:32 +0700

    Update list_freeipa-users.sh
    update script

--- list_freeipa-users.sh
+++ list_freeipa-users.sh
@@ -3,10 +3,10 @@
 
 ipa user-find --all --sizelimit=0 | awk '
 BEGIN {
-  format="| %-18s | %-12s | %-12s | %-8s | %-10s |\n"
-  line="|--------------------|--------------|--------------|----------|------------|"
+  format="| %-18s | %-12s | %-12s | %-8s | %-10s | %-10s |\n"
+  line="|--------------------|--------------|--------------|----------|------------|------------|"
   print line
-  printf format, "UserLogin", "FirstName", "LastName", "Status", "UID"
+  printf format, "UserLogin", "FirstName", "LastName", "Status", "UID", "GID"
   print line
   count=0
 }
@@ -16,20 +16,18 @@ BEGIN {
   for(i=4;i<=NF;i++) fullname=fullname " " $i
 }
 /Last name:/ {lastname=$3}
-/UID:/ {
-  uid=$2
+/UID:/ {uid=$2}
+/GID:/ {
+  gid=$2
   split(fullname, arr, " ")
   firstname=arr[1]
   status="enabled"
-  printf format, login, firstname, lastname, status, uid
+  printf format, login, firstname, lastname, status, uid, gid
   count++
-  login=""; firstname=""; lastname=""; fullname=""; uid=""
+  login=""; firstname=""; lastname=""; fullname=""; uid=""; gid=""
 }
 END {
   print line
-  printf("| Total Users: %-55d |\n", count)
+  printf("| Total Users: %-67d |\n", count)
   print line
 }'
-
-
-
